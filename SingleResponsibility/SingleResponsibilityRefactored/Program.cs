6b76cd280c9b21a724003e524682b22fe7174417
Aristotle <Aristotle@jlbthefarm.onmicrosoft.com>
2020-02-12 09:22:23 -0600

    Initialization of single responsibility refactor

---
+++ SingleResponsibility/SingleResponsibilityRefactored/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingleResponsibilityRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
